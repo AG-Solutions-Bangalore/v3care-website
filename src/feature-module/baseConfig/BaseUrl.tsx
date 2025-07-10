62a1d7b4c38d66b3403123e2b81e61401a8182d9
Sajid-tech <s.khan9430319425@gmail.com>
2025-07-10 15:43:57 +0530

    `base url changed`

--- src/feature-module/baseConfig/BaseUrl.tsx
+++ src/feature-module/baseConfig/BaseUrl.tsx
@@ -1,28 +1,28 @@
 // Base API URLs
-const BASE_URL = "https://agscare.site/crmapi/public";
-const BASE_URL_PINCODE = "https://api.v3care.in";
-const NO_IMAGE_URL = "https://agscare.site/crmapi/storage/app/public/no_image.jpg"
-const CLIENT_IMAGE_URL = "https://agscare.site/crmapi/storage/app/public/clients";
-const SERVICE_IMAGE_URL = "https://agscare.site/crmapi/storage/app/public/service";
-const SERVICE_DETAILS_IMAGE_URL = "https://agscare.site/crmapi/storage/app/public/service_details";
-const SERVICE_SUB_IMAGE_URL = "https://agscare.site/crmapi/storage/app/public/service_sub";
-const SERVICE_SUPER_IMAGE_URL = "https://agscare.site/crmapi/storage/app/public/service_super";
-const TESTIMONIAL_IMAGE_URL = "https://agscare.site/crmapi/storage/app/public/testimonial";
-const BLOG_IMAGE_URL = "https://agscare.site/crmapi/storage/app/public/blog";
+// const BASE_URL = "https://agscare.site/crmapi/public";
+// const BASE_URL_PINCODE = "https://api.v3care.in";
+// const NO_IMAGE_URL = "https://agscare.site/crmapi/storage/app/public/no_image.jpg"
+// const CLIENT_IMAGE_URL = "https://agscare.site/crmapi/storage/app/public/clients";
+// const SERVICE_IMAGE_URL = "https://agscare.site/crmapi/storage/app/public/service";
+// const SERVICE_DETAILS_IMAGE_URL = "https://agscare.site/crmapi/storage/app/public/service_details";
+// const SERVICE_SUB_IMAGE_URL = "https://agscare.site/crmapi/storage/app/public/service_sub";
+// const SERVICE_SUPER_IMAGE_URL = "https://agscare.site/crmapi/storage/app/public/service_super";
+// const TESTIMONIAL_IMAGE_URL = "https://agscare.site/crmapi/storage/app/public/testimonial";
+// const BLOG_IMAGE_URL = "https://agscare.site/crmapi/storage/app/public/blog";
 
 
 
 // actual url 
-// const BASE_URL = "https://v3care.com/crmapi/public";
-// const BASE_URL_PINCODE = "https://api.v3care.in";
-// const NO_IMAGE_URL = "https://v3care.com/crmapi/storage/app/public/no_image.jpg"
-// const CLIENT_IMAGE_URL = "https://v3care.com/crmapi/storage/app/public/clients";
-// const SERVICE_IMAGE_URL = "https://v3care.com/crmapi/storage/app/public/service";
-// const SERVICE_DETAILS_IMAGE_URL = "https://v3care.com/crmapi/storage/app/public/service_details";
-// const SERVICE_SUB_IMAGE_URL = "https://v3care.com/crmapi/storage/app/public/service_sub";
-// const SERVICE_SUPER_IMAGE_URL = "https://v3care.com/crmapi/storage/app/public/service_super";
-// const TESTIMONIAL_IMAGE_URL = "https://v3care.com/crmapi/storage/app/public/testimonial";
-// const BLOG_IMAGE_URL = "https://v3care.com/crmapi/storage/app/public/blog";
+const BASE_URL = "https://v3care.com/crmapi/public";
+const BASE_URL_PINCODE = "https://api.v3care.in";
+const NO_IMAGE_URL = "https://v3care.com/crmapi/storage/app/public/no_image.jpg"
+const CLIENT_IMAGE_URL = "https://v3care.com/crmapi/storage/app/public/clients";
+const SERVICE_IMAGE_URL = "https://v3care.com/crmapi/storage/app/public/service";
+const SERVICE_DETAILS_IMAGE_URL = "https://v3care.com/crmapi/storage/app/public/service_details";
+const SERVICE_SUB_IMAGE_URL = "https://v3care.com/crmapi/storage/app/public/service_sub";
+const SERVICE_SUPER_IMAGE_URL = "https://v3care.com/crmapi/storage/app/public/service_super";
+const TESTIMONIAL_IMAGE_URL = "https://v3care.com/crmapi/storage/app/public/testimonial";
+const BLOG_IMAGE_URL = "https://v3care.com/crmapi/storage/app/public/blog";
 
 export {
   BASE_URL,
